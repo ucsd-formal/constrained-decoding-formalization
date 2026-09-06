18664e1e89342710f858576de33e352ad167c9e9
enigmurl <43832426+enigmurl@users.noreply.github.com>
2026-09-06 13:39:59 -0400

    rename mask baseline to allowed

--- ConstrainedDecodingFormalization/GCDAlgorithm.lean
+++ ConstrainedDecodingFormalization/GCDAlgorithm.lean
@@ -81,17 +81,17 @@ def PreprocessParser (fst_comp : FST α Γ σa) (p : PDA Γ π σp) : PPTable α
 and current parser stack. -/
 def ComputeValidTokenMask (P : PDA Γ π σp) (itst : List Γ → σa → List α)
   (table : PPTable α σp σa Γ) (qa : σa) (qp : σp) (st : List π) : List α :=
-  let accepted := (table qp qa).fst
+  let allowed := (table qp qa).fst
   let dependent := (table qp qa).2.1
-  let accepted :=
+  let allowed :=
     dependent.foldl
       (fun acc d =>
         if (P.evalFrom {(qp, st)} d) ≠ ∅ then
           acc ++ (itst d qa)
         else
           acc)
-      accepted
-  accepted.dedup
+      allowed
+  allowed.dedup
 
 /-! ### Full GCD checker assembly -/
 
